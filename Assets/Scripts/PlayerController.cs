c3acbfd66e96969764cb8afc7615c8fd649dc22b
THerbron <tanguy.herbron@outlook.com>
2019-09-26 21:53:40 +0200

    Major player improvement
    - Fix fly
    - New sprite
    - New jump animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,35 +9,41 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
 
     private Rigidbody2D rigi2D;
-    private bool isJumping;
+    private Collider2D coll2D;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private bool isFacingForward;
+
+    public LayerMask groundMask;
+
     // Start is called before the first frame update
     void Start()
     {
         rigi2D = GetComponent<Rigidbody2D>();
-        isJumping = false;
+        coll2D = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        isFacingForward = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(rigi2D.velocity.y == 0)
-        {
-            isJumping = false;
-        }
-
         if(Input.GetAxis("Horizontal") != 0)
         {
+            spriteRenderer.flipX = Input.GetAxis("Horizontal") < 0;
+
             rigi2D.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigi2D.velocity.y);
         }
 
-        if(Input.GetAxis("Jump") != 0 && !isJumping)
+        if(Input.GetAxis("Jump") != 0 && Physics2D.IsTouchingLayers(coll2D, groundMask))
         {
-            isJumping = true;
-
             rigi2D.velocity = new Vector2(rigi2D.velocity.x, jumpForce);
         }
 
-        Debug.Log(">> " + rigi2D.velocity.y);
-        
+        animator.SetFloat("Speed", Mathf.Abs(rigi2D.velocity.x));
+        animator.SetFloat("VelocityY", rigi2D.velocity.y);
     }
 }
